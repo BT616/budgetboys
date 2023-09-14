6102da6d5bde2686b335943a7198c9f757cc6b9e
BT616 <110855674+BT616@users.noreply.github.com>
2023-09-14 14:46:37 -0700

    html,js,css setup and first input for budget

---
+++ script.js
@@ -0,0 +1,8 @@
+let totalAmount = document.getElementById("total-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+
+
+totalAmountButton.addEventListener("click" , () =>{
+   
+   console.log(totalAmount.value)
+} ); 
